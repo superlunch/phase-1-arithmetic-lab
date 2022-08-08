38a39dd21d9211898b82bea31f8ced901e505b40
Scout Cox <coxscout@gmail.com>
2022-08-07 20:56:15 -0400

    commit tree 1b0f2e7187fcbf8a108626c31b836abc9921638f

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 = 2
+const num2 = 31
+const num3 = 6
+const num4 = 16
 
-// Write your code here
+const multiply = num1 * num2 
+const random = Math.floor(Math.random()*1000)+1
+const mod = num4 % num3
+const max = Math.max(1, 2, 5, 10, 20)
